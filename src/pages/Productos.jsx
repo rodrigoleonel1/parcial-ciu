95c107c2a73e8f5b8200476f509a1ddcd41a2b2d
Rodrigo Alarcón <rodricsxd@gmail.com>
2026-06-01 18:52:15 -0300

    Primer commit

---
+++ src/pages/Productos.jsx
@@ -0,0 +1,12 @@
+export const productos = [
+  {
+    id: 1,
+    nombre: "",
+    categoria: "",
+    precio: 1000,
+    imagen: "",
+    descripcion: "",
+    stock: 10,
+    etiqueta: "",
+  },
+];
