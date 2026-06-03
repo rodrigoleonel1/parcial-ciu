cb7d59a2568b852725d1316981261553a8b7fd99
David Acosta <davidisrraelacostaz.9@gmail.com>
2026-06-03 19:05:01 -0300

    agregando funciones basicas en contexto esperando detalles de producto para poder interactuar

--- src/context/CarritoContext.jsx
+++ src/context/CarritoContext.jsx
@@ -1,10 +1,58 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export const CarritoContext = createContext([]);
+export const CarritoContext = createContext();
+
+//Agregar carrito (se dispara dentro detalle con el btn agregar carrito)
+
+//Eliminar carrito (se dispara dentro de carrito con el boton de la card
+//  renderizada se mostrar un boton en la card que diga eliminar y
+//  que pueda elegir la cantidad de unidades)
+
+//calcular total de productos (automatico)
+
+//calcular precio total del producto (automatico)
 
 const CarritoProvider = ({ children }) => {
+
+    const [carrito, setCarrito] = useState([]);
+
+    function agregarProducto() {
+        //logica
+    }
+
+    function eliminarProducto() {
+        //logica
+    }
+
+
+    function aumentarCantidad() {
+        //logica
+    }
+
+
+    function disminuirCantidad() {
+        //logica
+    }
+
+    function totalCantidad() {
+        //logica
+    }
+
+
+    function totalPrecio() {
+        //logica
+    }
+
     return (
-        <CarritoContext.Provider value={{}}>
+        <CarritoContext.Provider value={{
+            carrito,
+            agregarProducto,
+            eliminarProducto,
+            aumentarCantidad,
+            disminuirCantidad,
+            totalCantidad,
+            totalPrecio
+        }}>
             {children}
         </CarritoContext.Provider>
     );
