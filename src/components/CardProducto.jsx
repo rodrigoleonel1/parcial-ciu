8cbb2e26de40918f0f6680d6e22150f496924533
lucaCarlino04 <lucacarlinounahur@gmail.com>
2026-06-03 17:23:16 -0300

    Añadi las tarjetas al catalogo correctamente importadas junto con el uso de su contexto

--- src/components/CardProducto.jsx
+++ src/components/CardProducto.jsx
@@ -1,8 +1,15 @@
 
-
 export default function CardProducto({ producto }) {
-  return <div className="relative border h-40">
-    <img src={producto.imagen} alt={producto.nombre} className="w-full h-full object-cover" />
-    
+  return <div className="relative  h-40">
+    <img src={producto.imagen} alt={producto.nombre} className="w-full h-full object-cover rounded-lg" />
+
+    <div className="absolute bottom-3 left-3 right-3">
+      <div className="flex justify-between">
+        <span>{producto.nombre}</span>
+        <span>{producto.precio}</span>
+      </div>
+
+      <p>{producto.categoria}</p>
+    </div>
   </div>;
 }
--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,5 +1,20 @@
+import { useContext } from "react";
+import { ProductosContext } from "../context/ProductosContext";
+import CardProducto from "../components/CardProducto"
+
 export default function Catalogo() {
+  const {productos} = useContext(ProductosContext);
+
+    console.log(productos);
+
   return (
-    <div>Catalogo</div>
+<div className="container mx-auto py-8">
+  <h1 className="text-2xl font-bold mb-4">Catálogo de Productos</h1>
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    {productos.map(producto => <CardProducto key={producto.id} producto={producto} />)}
+  </div></div>
   )
 }
+/*
+
+  */
