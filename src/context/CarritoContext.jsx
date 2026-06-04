ed677d4bb70f69854626271a17cab5e0bb7c1f51
David555acosta <148257651+David555acosta@users.noreply.github.com>
2026-06-04 03:21:05 -0300

    se modifico metodo agregar carrito antes permitia agregar multiples a la vez , se puede utilizar la anterior logica para otra compra , se diseño estructura basica de itemCarrito

--- src/context/CarritoContext.jsx
+++ src/context/CarritoContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useState } from "react";
-
 export const CarritoContext = createContext();
 
 //Agregar carrito (se dispara dentro detalle con el btn agregar carrito)
@@ -15,7 +14,7 @@ export const CarritoContext = createContext();
 const CarritoProvider = ({ children }) => {
   const [carrito, setCarrito] = useState([]);
 
-  function agregarProducto(producto) {
+  function agregarVariosProductos(producto) {
     const existe = carrito.find((p) => p.id === producto.id);
     if (existe) {
       if (existe.cantidad < producto.stock) {
@@ -25,8 +24,17 @@ const CarritoProvider = ({ children }) => {
           ),
         );
       } else {
-        alert("No hay más stock");
+        alert("Excedio el limite de  compra");
       }
+    } else {
+      setCarrito([...carrito, { ...producto, cantidad: 1 }]);
+    }
+  }
+
+  function agregarProducto(producto) {
+    const existe = carrito.find((p) => p.id === producto.id);
+    if (existe) {
+      alert("No se puede comprar mas de un juego a la vez");
     } else {
       setCarrito([...carrito, { ...producto, cantidad: 1 }]);
     }
@@ -51,7 +59,9 @@ const CarritoProvider = ({ children }) => {
   }
 
   function totalPrecio() {
-    //logica
+    return carrito.reduce((acc, curr) => {
+      return acc + curr.cantidad + curr.precio;
+    }, 0);
   }
 
   return (
--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import { CarritoContext } from "../context/CarritoContext";
+import { ProductosContext } from "../context/ProductosContext";
+import ItemCarrito from "../components/ItemCarrito";
 
 export default function Carrito() {
   const { carrito, agregarProducto, totalProductos, totalPrecio } =
@@ -26,21 +28,21 @@ export default function Carrito() {
             <div className="bg-white shadow rounded-lg p-4">
               <h2 className="font-semibold text-lg">Nombre del producto</h2>
 
-              <p className="text-sm text-gray-500">Precio: $0</p>
-              <p className="text-sm text-gray-500">Cantidad: 0</p>
-              <p className="text-sm font-medium mt-1">Subtotal: $0</p>
-
-              <div className="flex gap-2 mt-3">
-                <button className="bg-red-500 text-white px-3 rounded ml-auto">
-                  Eliminar
-                </button>
-              </div>
+              <p className="text-sm text-gray-500">
+                Precio total: {totalPrecio()}
+              </p>
             </div>
           </div>
-          <h3 className="font-semibold text-lg t">
-            Aca voy a renderizar unas cards con los productos comprados que van
-            a ser dinamicos
-          </h3>
+          <div className="container mx-auto py-8">
+            <h1 className="text-2xl font-bold mb-4 bg-black">
+              Detalles de juego comprado 
+            </h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              {carrito.map((producto) => (
+                <ItemCarrito key={producto.id} producto={producto} />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </>
