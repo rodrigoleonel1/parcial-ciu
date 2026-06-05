12366e4cca200b8909e467fb526847a8a5ea5dfd
David555acosta <148257651+David555acosta@users.noreply.github.com>
2026-06-05 02:52:26 -0300

    se agregaron estilos al carrito permitiendo visualizar productos siempre en columna , corregir div que contiene productos en resoluciones grandes tira hacia la izquierda

--- src/components/ItemCarrito.jsx
+++ src/components/ItemCarrito.jsx
@@ -1,22 +1,46 @@
 import { useContext } from "react";
 import { CarritoContext } from "../context/CarritoContext";
 
+import {
+  Target,
+  Eye,
+  Heart,
+  Headset,
+  Truck,
+  ShieldCheck,
+  Star,
+  Users,
+  Package,
+  Zap,
+} from "lucide-react";
+
+const estiloDetallesGenerales = {
+  icon: <ShieldCheck className="w-7 h-7 text-cyan-400" />,
+  titulo: "Compra segura",
+  descripcion:
+    "Todos nuestros pagos están protegidos con encriptación SSL. Tu información nunca es compartida con terceros.",
+};
+
 export default function ItemCarrito({ producto }) {
-  const { eliminarProducto, agregarProducto, carrito } = useContext(CarritoContext);
+  const { eliminarProducto, agregarProducto, carrito } =
+    useContext(CarritoContext);
   const existeProductoActual = carrito.find((p) => p.id === producto.id);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-5">
       <div
         key={producto.id}
-        className="bg-white shadow rounded-lg p-4 flex gap-4 items-center"
+        className="bg-gray-900 border border-gray-800 shadow-lg 
+        rounded-xl p-4 flex flex-col sm:flex-row gap-4 sm:items-center hover:border-cyan-400 transition-all"
       >
         <img
           src={producto.imagen}
           alt={producto.nombre}
           className="w-24 h-24 object-cover rounded-lg"
         />
-        <div className="flex-1">
-          <h2 className="font-semibold text-lg">{producto.nombre}</h2>
+        <div className="flex-1 min-w-0">
+          <h2 className="font-semibold text-lg text-white break-words">
+            {producto.nombre}
+          </h2>
 
           <p className="text-sm text-gray-500">Precio: ${producto.precio}</p>
 
@@ -27,23 +51,21 @@ export default function ItemCarrito({ producto }) {
           </p>
         </div>
         <div className="flex flex-col gap-2">
-          {
-            existeProductoActual ? (
-              <button
-                className="bg-red-500 text-white px-3 py-2 rounded hover:bg-red-600 hover:cursor-pointer transition-colors"
-                onClick={() => eliminarProducto(producto.id)}
-              >
-                Eliminar
-              </button>
-            ) : (
-              <button
-                className="bg-green-500 text-white px-3 py-2 rounded hover:bg-green-600 hover:cursor-pointer transition-colors"
-                onClick={() => agregarProducto(producto)}
-              >
-                Agregar
-              </button>
-            )
-          }
+          {existeProductoActual ? (
+            <button
+              className="bg-red-500 text-white px-2 py-1 text-xs sm:px-3 sm:py-2 sm:text-sm rounded hover:bg-red-600 hover:cursor-pointer transition-colors"
+              onClick={() => eliminarProducto(producto.id)}
+            >
+              Eliminar
+            </button>
+          ) : (
+            <button
+              className="bg-blue-500 text-white px-2 py-1 text-xs sm:px-3 sm:py-2 sm:text-sm rounded hover:bg-blue-600 hover:cursor-pointer transition-colors"
+              onClick={() => agregarProducto(producto)}
+            >
+              Agregar
+            </button>
+          )}
         </div>
       </div>
     </div>
--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,53 +1,40 @@
 import { useContext } from "react";
 import { CarritoContext } from "../context/CarritoContext";
-import { ProductosContext } from "../context/ProductosContext";
 import ItemCarrito from "../components/ItemCarrito";
 
 export default function Carrito() {
-  const {carrito , carritoAux, totalProductos, totalPrecio } =
+  const { carritoAux, totalProductos, totalPrecio } =
     useContext(CarritoContext);
+
   return (
-    <>
-      <div>Carrito</div>
-      <div className="bg-gray-100 min-h-screen p-6">
-        <div className="max-w-5xl mx-auto">
-          <div className="bg-white shadow rounded-lg p-4 mb-6 flex justify-between items-center">
-            <div>
-              <h1 className="text-xl font-bold">Carrito</h1>
-              <p className="text-gray-500 text-sm">
+    <section className="bg-linear-to-b from-cyan-950/30 via-gray-950 to-gray-950 py-20 min-h-screen">
+      <div className="max-w-5xl mx-auto px-4">
+        <h1 className="font-display font-bold text-4xl sm:text-6xl lg:text-7xl text-center mb-5">
+          Tus compras
+        </h1>
+        <div className="rounded-lg p-4 mb-6 flex flex-col items-center gap-2 text-white hover:border-cyan-400 transition-all">
+          {carritoAux.length === 0 ? (
+            <div className="bg-linear-to-b from-cyan-950/10 via-gray-950 to-gray-950 py-20 min-h-screen shadow rounded-lg p-8 text-center">
+              <p className="text-white-500 text-lg">¡Tu carrito está vacío !</p>
+            </div>
+          ) : (
+            <div className="text-center">
+              <p className="text-gray-300 text-sm">
                 Productos totales: {totalProductos()}
               </p>
+              <p className="text-gray-300 text-sm">Total: ${totalPrecio()}</p>
+              <button className="bg-gray-700 text-white px-5 py-2 text-xs sm:px-3 sm:py-2 sm:text-sm rounded hover:bg-green-600 hover:cursor-pointer transition-colors m-2">
+                Continuar compra
+              </button>
             </div>
-
-            <div className="text-right">
-              <p className="text-gray-500 text-sm">Total</p>
-              <p className="text-2xl font-bold">$0</p>
-            </div>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="bg-white shadow rounded-lg p-4">
-              <h2 className="font-semibold text-lg">Nombre del producto</h2>
-
-              <p className="text-sm text-gray-500">
-                Precio total: {totalPrecio()}
-              </p>
-            </div>
-          </div>
-          <div className="container mx-auto py-8">
-            <h1 className="text-2xl font-bold mb-4 bg-black">
-              Detalles de juego comprado
-            </h1>
-            <button className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600" onClick={() => alert(carritoAux.length)}>DEBUG CARRITO AUX</button>
-            <button className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-gray-600" onClick={() => alert(carrito.length)}>DEBUG CARRITO PRINCIPAL</button>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {carritoAux.map((producto) => (
-                <ItemCarrito key={producto.id} producto={producto} />
-              ))}
-            </div>
-          </div>
+          )}
+        </div>
+        <div className="space-y-4">
+          {carritoAux.map((producto) => (
+            <ItemCarrito key={producto.id} producto={producto} />
+          ))}
         </div>
       </div>
-    </>
+    </section>
   );
 }
