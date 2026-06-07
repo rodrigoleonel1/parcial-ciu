6237def01a29faaa5ff1b0f70141514ebec68006
OviedoAgustin <agustinleandrooviedou@gmail.com>
2026-06-07 03:50:55 -0300

    Se completo el esqueleto de la pagina para finalizar compra. Falta añadir funcionalidades

--- src/pages/FinalizarCompra.jsx
+++ src/pages/FinalizarCompra.jsx
@@ -5,7 +5,7 @@ export default function FinalizarCompra() {
         <section className="bg-linear-to-b from-cyan-950/30 via-gray-950 to-gray-950 py-20 min-h-screen">
             <div className="max-w-2xl mx-auto px-4">
                 <h1 className="font-display font-bold text-4xl sm:text-6xl text-center mb-10 text-white">
-                    Finalizar Compra
+                    Finalizar <span className="text-cyan-400">Compra</span>
                 </h1>
                 <FormularioCompra />
             </div>
